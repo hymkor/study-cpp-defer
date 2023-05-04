11bec747e208b6844c32b472d049b2efb05a143d
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-04 13:44:39 +0900

    Define `defer` as a macro

--- defer.h
+++ defer.h
@@ -5,3 +5,7 @@ public:
     End(const T &f_) : f(f_){}
     ~End(){ f(); }
 };
+
+#define CONCATINATE_IMPLEMENT(x,y) x ## y
+#define CONCATINATE(x,y) CONCATINATE_IMPLEMENT(x,y)
+#define defer End CONCATINATE(defer,__LINE__)=
--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include "defer.h"
 
 int main(){
-    End end = [](){ std::cout << "defer code" << std::endl; };
+    End end = [](){ std::cout << "defer code(1)" << std::endl; };
+    defer [](){ std::cout << "defer code(2)" << std::endl; };
+    defer [](){ std::cout << "defer code(3)" << std::endl; };
 
     std::cout << "main code" << std::endl;
     return 0;
